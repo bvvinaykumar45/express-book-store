3d23fc2b70b3b2955aa938e03a30061728ddf2ab
bvvinaykumar45 <bvvinaykumar45@gmail.com>
2026-05-01 18:19:34 +0530

    Step 5. Set Up GET route to Fetch a Book by ID

--- index.js
+++ index.js
@@ -14,4 +14,20 @@ app.get('/books', (req, res) => {
   res.json(books);
 });
 
+app.get('/books/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+
+  if(isNaN(id))
+    return res.status(400).json({error: `id must be of type number`});
+  
+  const book = books.find(book => book.id === id); // SELECT * FROM books WHERE id = :id
+
+  if(!book)
+    return res
+      .status(404)
+      .json({error: `Book with id ${id} does not exists!`});
+
+  return res.json(book);
+});
+
 app.listen(PORT, () => console.log(`Http server is running on PORT ${PORT}`));
