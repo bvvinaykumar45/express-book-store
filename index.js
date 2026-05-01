7e9cc4df0d0f02854506e66e575269bcb84bb2a1
bvvinaykumar45 <bvvinaykumar45@gmail.com>
2026-05-01 17:54:31 +0530

    Step 3: Create Book Data

--- index.js
+++ index.js
@@ -3,4 +3,10 @@ const express = require('express');
 const app = express();
 const PORT = 8000;
 
+// In memory DB
+const books = [
+  {id: 1, title: 'Title One', author: 'Author One'},
+  {id: 1, title: 'Title Two', author: 'Author Two'},  
+];
+
 app.listen(PORT, () => console.log(`Http server is running on PORT ${PORT}`));
