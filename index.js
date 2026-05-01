b06d01fa37920537644696fb374870e29ae417ed
bvvinaykumar45 <bvvinaykumar45@gmail.com>
2026-05-01 18:49:47 +0530

    Step 6. Set Up POST Route to Add a New Book

--- index.js
+++ index.js
@@ -5,29 +5,54 @@ const PORT = 8000;
 
 // In memory DB
 const books = [
-  {id: 1, title: 'Title One', author: 'Author One'},
-  {id: 1, title: 'Title Two', author: 'Author Two'},  
+  { id: 1, title: 'Title One', author: 'Author One' },
+  { id: 1, title: 'Title Two', author: 'Author Two' },  
 ];
+
+// Middlewares (Plugins)
+app.use(express.json());
 
 // Routes
 app.get('/books', (req, res) => {
-  res.json(books);
+  return res.json(books);
 });
 
 app.get('/books/:id', (req, res) => {
   const id = parseInt(req.params.id);
 
   if(isNaN(id))
-    return res.status(400).json({error: `id must be of type number`});
-  
+    return res.status(400).json({ error: `id must be of type number` });
+
   const book = books.find(book => book.id === id); // SELECT * FROM books WHERE id = :id
 
   if(!book)
     return res
       .status(404)
-      .json({error: `Book with id ${id} does not exists!`});
+      .json({ error: `Book with id ${id} does not exists!` });
 
   return res.json(book);
+});
+
+app.post('/books', (req, res) => {
+  const { title, author } = req.body;
+  
+  if(!title || title === '')
+    return res.status(400).json({ error: 'title is required' });
+  
+  if(!author || author === '')
+    return res.status(400).json({ error: 'author is required' });
+
+  const id = books.length + 1;
+
+  const book = {
+    id,
+    title,
+    author
+  };
+
+  books.push(book);
+
+  return res.status(201).json({ message: 'Book created success', id });
 });
 
 app.listen(PORT, () => console.log(`Http server is running on PORT ${PORT}`));
